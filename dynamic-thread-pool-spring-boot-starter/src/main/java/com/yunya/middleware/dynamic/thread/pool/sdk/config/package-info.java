f750ec0999a3cb4c19fe9ba5858512d62b1acd29
yunyala <892722202@qq.com>
2024-07-10 14:22:53 +0800

    package-info.java 文件作用是用来为 Java 包提供注释的特殊文件，非必须

--- dynamic-thread-pool-spring-boot-starter/src/main/java/com/yunya/middleware/dynamic/thread/pool/sdk/config/package-info.java
+++ dynamic-thread-pool-spring-boot-starter/src/main/java/com/yunya/middleware/dynamic/thread/pool/sdk/config/package-info.java
@@ -1,4 +1,6 @@
 /**
+ * package-info.java 文件是用来为 Java 包提供注释的特殊文件。它并不包含类或者接口的定义，
+ * 而是用来为整个包添加注释、声明包级别的注解以及提供包的文档信息。
  * auto config 配置入口
  */
 package com.yunya.middleware.dynamic.thread.pool.sdk.config;
